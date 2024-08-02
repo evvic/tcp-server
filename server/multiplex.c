31da7554d25413ed00ac82eed34544574a4db9c2
Eric <ewic98@gmail.com>
2024-08-02 00:08:51 -0700

    handle multiple clients in loop

--- server/multiplex.c
+++ server/multiplex.c
@@ -90,7 +90,7 @@ void init_tcp_server()
         addr_len = 0,
         opt = 1;
 
-    int comm_socket_fd = 0;     // client specific comm socket file descriptor
+    // int comm_socket_fd = 0;     // client specific comm socket file descriptor
 
     fd_set readfds;             // set of FDs which select() polls
 
@@ -165,7 +165,7 @@ void init_tcp_server()
 
             /* Create a temp file descriptor for the rest of the connections life */
 
-            comm_socket_fd = accept(            // accept the connection and return the FD
+            int comm_socket_fd = accept(        // accept the connection and return the FD
                 master_socket_fd,               // master FD only used for accepting the new clients connection
                 (struct sockaddr*)&client_addr, // pass empty client_addr to be populated with IP address & port
                 &addr_len                       // const size of sockaddr
@@ -196,69 +196,79 @@ void init_tcp_server()
         }
         else                                    // data arrived on a client communication socket FD
         {
-
-            while(1)
+            int comm_socket_fd = -1;
+            for (int i = 0; i < MAX_SUPPORTED_CLIENTS; i++)
             {
-                printf("Server ready to service client msgs.\n");
-                memset(                         // prepare memory space for server to store data
-                    DATA_BUFFER,                // received from the client of size DATA_BUFFER
-                    0,
-                    sizeof(DATA_BUFFER));
-
-                /* Server receiving data from the client */
-
-                sent_recv_bytes = recvfrom(     // a blocking system call until data arrives at comm_socket_fd
-                    comm_socket_fd,             // all communciation happens on the communication FD (not master FD)
-                    (char*)DATA_BUFFER,         // the location which the data is going to be stored
-                    sizeof(DATA_BUFFER),        // how many bytes long is this data buffer
-                    0,
-                    (struct sockaddr*)&client_addr,
-                    sizeof(struct sockaddr)
-                );
-
-                printf("Server received %d bytes from client %s:%u\n",
-                    sent_recv_bytes,
-                    ip_str, // uint32_t or unsigned int
-                    ntohs(client_addr.sin_port));
-
-                if (sent_recv_bytes == 0)       // if server received 0 bytes from client
-                {                               // server may close the connection and wait for
-                    close(comm_socket_fd);      // a new conenction
-                    break;
-                }
-
-                test_data *client_data = (test_data *)DATA_BUFFER;
-
-                if (client_data->a == 0 &&
-                    client_data->b == 0)
+                if (FD_ISSET(                   // find which comm socket FD is active in monitored_fd_set
+                    monitored_fd_set[i],        // use the array of FD integers to check which one
+                    &readfds                    // is active in readfds
+                ))
                 {
-                    close(comm_socket_fd);
-                    printf("Server closed connection with client %s:%u\n",
-                        ip_str,
+                    comm_socket_fd = monitored_fd_set[i];
+                    memset(                         // prepare memory space for server to store data
+                        DATA_BUFFER,                // received from the client of size DATA_BUFFER
+                        0,
+                        sizeof(DATA_BUFFER));
+
+                    /* Server receiving data from the client */
+
+                    sent_recv_bytes = recvfrom(     // a blocking system call until data arrives at comm_socket_fd
+                        comm_socket_fd,             // all communciation happens on the communication FD (not master FD)
+                        (char*)DATA_BUFFER,         // the location which the data is going to be stored
+                        sizeof(DATA_BUFFER),        // how many bytes long is this data buffer
+                        0,
+                        (struct sockaddr*)&client_addr,
+                        sizeof(struct sockaddr)
+                    );
+
+                    printf("Server received %d bytes from client %s:%u\n",
+                        sent_recv_bytes,
+                        ip_str, // uint32_t or unsigned int
                         ntohs(client_addr.sin_port));
 
-                    break;
+                    if (sent_recv_bytes == 0)       // if server received 0 bytes from client
+                    {                               // server may close the connection and wait for
+                        close(comm_socket_fd);      // a new conenction
+                        remove_from_monitored_fd_set(comm_socket_fd);
+                        break;
+                    }
+
+                    test_data *client_data = (test_data *)DATA_BUFFER;
+
+                    if (client_data->a == 0 &&      // End connection with client
+                        client_data->b == 0)        // if both a and b values are 0
+                    {
+                        close(comm_socket_fd);
+                        remove_from_monitored_fd_set(comm_socket_fd);
+                        printf("Server closed connection with client %s:%u\n",
+                            ip_str,
+                            ntohs(client_addr.sin_port));
+
+                        break;
+                    }
+
+                    result_data result;
+
+                    /* Server is calculating the sum of values a + b */
+                    result.c = client_data->a + client_data->b;
+
+                    /* Server replying back to client */
+
+                    sent_recv_bytes = sendto(       // server is sending the result back to the client
+                        comm_socket_fd,             // invoked on the comm_socket_fd to reply to cleint
+                        (char*)&result,
+                        sizeof(result_data),
+                        0,
+                        (struct sockaddr*)&client_addr,
+                        sizeof(struct sockaddr)
+                    );
+
+                    printf("Server send %d bytes in reply to client\n",
+                        sent_recv_bytes
+                    );
+
+
                 }
-
-                result_data result;
-
-                /* Server is calcualting the sum of values a + b */
-                result.c = client_data->a + client_data->b;
-
-                /* Server replying back to client */
-
-                sent_recv_bytes = sendto(       // server is sending the result back to the client
-                    comm_socket_fd,             // invoked on the comm_socket_fd to reply to cleint
-                    (char*)&result,
-                    sizeof(result_data),
-                    0,
-                    (struct sockaddr*)&client_addr,
-                    sizeof(struct sockaddr)
-                );
-
-                printf("Server send %d bytes in reply to client\n",
-                    sent_recv_bytes
-                );
             }
         }
     }
